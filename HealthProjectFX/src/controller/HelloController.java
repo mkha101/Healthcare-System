eabda5381130c34e68d02ed0dbfca39b4aa970cd
mkha101 <92785420+mkha101@users.noreply.github.com>
2023-02-13 20:18:03 -0500

    Add files via upload
    addPatient controller file and FXML, GUI and functionality

---
+++ HealthProjectFX/src/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.example.hospitalmanager;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+    @FXML
+    private TextField fName;
+    @FXML
+    private TextField lName;
+    @FXML
+    private DatePicker dob;
+    @FXML
+    private TextField medHistory;
+    @FXML
+    private TextField notes;
+    @FXML
+    private Button addPatientBtn;
+
+    private void addPatient(ActionEvent event) {
+        
+    }
+
+
+
+}
