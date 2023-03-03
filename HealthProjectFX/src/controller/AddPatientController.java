cd1a6de9828342d5ddf1585101b60a7ee388a864
JMaca <70349528+JMaca@users.noreply.github.com>
2023-03-02 21:00:55 -0500

    Changed tags to public.Fixed package name.Fixed File Name. Left methods to be finished at a later time.

---
+++ HealthProjectFX/src/controller/AddPatientController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class AddPatientController {
+    @FXML
+    public TextField fName;
+    @FXML
+    public TextField lName;
+    @FXML
+    public DatePicker dob;
+    @FXML
+    public TextField medHistory;
+    @FXML
+    public TextField notes;
+    @FXML
+    public Button addPatientBtn;
+
+//	public void addPatient(ActionEvent event) {
+//
+//	}
+
+}
+
